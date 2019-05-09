ad7b35595005af4b725116da80156d36e1d5ba63
martileos <martinevarez@gmail.com>
2019-05-09 13:59:43 -0600

    Fruta2 Vue.js

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -8,6 +8,43 @@ const app = new Vue({
 			{nombre:'Uva',cantidad: 0},
 			{nombre:'Peras',cantidad: 80},
 			{nombre:'Fresas',cantidad: 110}
-		]
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods: {
+		agregarFruta(){
+			// console.log("diste clic");
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad: 0
+			});
+			this.nuevaFruta = '';
+		}
+	},
+	computed: {
+		sumarFrutas(){
+			this.total = 0;
+			for(fruta of this.frutas2){
+				this.total=this.total+fruta.cantidad;
+				// this.total+=fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
